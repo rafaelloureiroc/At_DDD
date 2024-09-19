b1e63d53cc2f29dbcb72f45ad69a4739e991090e
Rafael Loureiro <93484572+rafaelloureiroc@users.noreply.github.com>
2024-09-19 18:37:45 -0300

    Add files via upload

---
+++ HistoricoPedido/src/test/java/com/AtDDD/HistoricoPedido/HistoricoPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AtDDD.HistoricoPedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricoPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
